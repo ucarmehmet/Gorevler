cb71c89e8468afd942bd0f8a702a793f7deb71dc
Mehmet UÇAR <ucarmemet1@gmail.com>
2025-01-15 17:49:12 +0300

    10 Ocak tarihinde paylaşılan görevler eklenen

---
+++ src/main/java/Ocak10/Task18_mDArraysAyniIndexToplamlari.java
@@ -0,0 +1,27 @@
+package Ocak10;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Task18_mDArraysAyniIndexToplamlari {
+    public static void main(String[] args) {
+
+        /*Aşağıdaki mutli dimensional array'lerin iç array'lerinde
+         aynı indexe sahip elamanların toplamını yazdıran bir program yazın.
+         arr1[]={{1,2},{3,4,5}{6}}
+         arr2[]={{7,8,9},{10,11},{12}} */
+
+        int[][] arr1 = {{1,2},{3,4,5},{6}};
+        int[][] arr2 = {{7,8,9},{10,11},{12}};
+        System.out.println("arr1 = "+Arrays.deepToString(arr1));
+        System.out.println("arr2 = "+Arrays.deepToString(arr2));
+
+        for (int i = 0; i < arr1.length && i < arr2.length; i++) {
+            for (int j = 0; j < arr1[i].length && j < arr2[i].length; j++) {
+
+                System.out.println("arr1 in ["+i+"]["+j+"] indeksi ile arr2 nin ["+i+"]["+j+"] indeksindeki sayıların toplamı = " + (arr1[i][j] + arr2[i][j]));
+
+            }
+        }
+    }
+}
